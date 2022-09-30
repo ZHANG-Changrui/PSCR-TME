538a6bee01c6e924bf122aec0e03034c3db2e183
ZHANG <fr.zhang.changrui@gmail.com>
2022-09-30 23:46:06 +0200

    question 3

--- TME2/tme2/main.cpp
+++ TME2/tme2/main.cpp
@@ -2,10 +2,20 @@
 #include <fstream>
 #include <regex>
 #include <chrono>
-int main () {
-	using namespace std;
-	using namespace std::chrono;
+using namespace std;
+using namespace std::chrono;
 
+bool cond(pair<string,int> p){
+    return p.first=="war";
+}
+void affiche3Occur(vector<pair<string,int>> v){
+    cout<<"taille vector: "<<v.size()<<endl;
+    for(auto i:v){
+        cout<<"|"<<i.first<<"|:"<<i.second<<endl;
+    }
+}
+
+int main () {
 	ifstream input = ifstream("WarAndPeace.txt");
 
 	auto start = steady_clock::now();
@@ -16,18 +26,40 @@ int main () {
 	string word;
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re( R"([^a-zA-Z])");
+    vector<pair<string,int>> v;
 
-    //exo2
-    vector<string> v;
 
 	while (input >> word) {
 		// élimine la ponctuation et les caractères spéciaux
 		word = regex_replace ( word, re, "");
         // passe en lowercase
         transform(word.begin(),word.end(),word.begin(),::tolower);
-
+/* exo2
         if(std::find(v.begin(),v.end(),word)==v.end()){
-            v.push_back(word);
+            v.push_back();
+        }
+*/
+
+
+        if(word=="toto"||word=="war"||word=="peace"){
+            if(v.empty()){
+                pair<string,int> tmp(word,1);
+                v.push_back(tmp);
+            }else{
+                const size_t sz=v.size();
+                bool t= false;
+                for(size_t i=0;i<sz;i++){
+                    if(v.at(i).first==word){
+                        t= true;
+                        pair<string,int> tmp(word,v.at(i).second+1);
+                        v.at(i)=tmp;
+                    }
+                }
+                if(!t){
+                    pair<string,int> tmp(word,1);
+                    v.push_back(tmp);
+                }
+            }
         }
 
 
@@ -35,9 +67,10 @@ int main () {
 
 
 		// word est maintenant "tout propre"
+        /*
 		if (nombre_lu % 100 == 0)
 			// on affiche un mot "propre" sur 100
-			cout << nombre_lu << ": "<< word << endl;
+			cout << nombre_lu << ": "<< word << endl;*/
 		nombre_lu++;
 	}
     input.close();
@@ -48,7 +81,9 @@ int main () {
               << duration_cast<milliseconds>(end - start).count()
               << "ms.\n";
     cout << "Found a total of " << nombre_lu << " words." << endl;
-    cout << "taille mots: " <<v.size()<<endl;
+    //cout << "taille mots: " <<v.size()<<endl;
+
+    affiche3Occur(v);
     return 0;
 }
 
