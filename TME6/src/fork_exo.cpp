5d52f0249b524210d4f1328306e3cc2249df0a79
21200428 <21200428@ppti-24-303-20.ufr-info-p6.jussieu.fr>
2022-10-28 11:23:39 +0200

    TM6 start

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <unistd.h>
-
+#include <wait.h>
+using namespace std;
 int main () {
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
-
+	int nbrfils=0;
 	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
 		std::cout << " i:j " << i << ":" << j << std::endl;
 		for (int k=1; k<=i && j==N ; k++) {
-			if ( fork() == 0) {
+			pid_t pid=fork();
+			if ( pid == 0) {
+				nbrfils=0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
+				cout<<"je suis :"<<getpid()<<endl;
+			}else{
+				nbrfils++;
+				int status;
+				wait(&status);
+				cout<<" je suit "<<getpid()<<" mon fils se termine. fils: "<<pid<<" "<<endl;
 			}
 		}
 	}
