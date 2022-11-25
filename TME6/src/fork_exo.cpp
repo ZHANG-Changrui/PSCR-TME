e87fb1c99e7caaddbabb3af21b741409b319ecdb
Changrui <m.zhang.changrui@gmail.com>
2022-11-25 13:49:34 +0100

    OK terminaison reussi SIGCHLD

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -13,12 +13,17 @@ void repos(){
     sleep(alea);
 }
 void handler(int signal){
-	--vie;
+    cout<<"attaque!"<<endl;
+
+    --vie;
     cout<<getpid()<<" handler! " <<vie<<endl;
+}
+void handlerwait(int signal){
+    wait(0);
 }
 int attaque(pid_t adversaire){
     //cout<<"attaque!"<<endl;
-	signal(SIGINT,handler);
+    signal(SIGINT,handler);
     return kill(adversaire,SIGINT);
 }
 void defense(){
@@ -26,37 +31,37 @@ void defense(){
     signal(SIGINT, SIG_IGN);
 }
 int combat(pid_t adversaire){
-	while(vie>0){
-        cout<<getpid()<<": combat vie: "<<vie<<endl;
+    while(vie>0){
+        //cout<<getpid()<<": combat vie: "<<vie<<endl;
         int killsig=attaque(adversaire);
-        cout<<"kill: "<<killsig<<" errno"<<errno<<endl;
+        //cout<<"kill: "<<killsig<<" errno"<<errno<<endl;
         if(killsig==-1){
-            cout<<getpid()<<" a gagné, "<<adversaire<<" a perdu!";
+            cout<<getpid()<<" a gagné, "<<adversaire<<" a perdu!"<<endl;
             return 0;
         }
         repos();
         defense();
         repos();
-	}
-    cout<<adversaire<<" a gagné, "<<getpid()<<" a perdu!";
+    }
+    cout<<adversaire<<" a gagné, "<<getpid()<<" a perdu!"<<endl;
     return 1;
 }
 int main () {
-	srand(time(nullptr));
+    srand(time(nullptr));
+    signal(SIGINT,SIG_IGN);
     pid_t vador=getpid();
     pid_t luke=fork();
-    signal(SIGINT,SIG_IGN);
     if(luke==-1){
         perror("luke");
         exit(1);
     }else if(luke==0){
-        cout<<"luke"<<endl;
+        cout<<"luke---------------"<<endl;
         int val=combat(vador);
-        exit(val);
-    }else{
-        cout<<"vador: "<<vador<<"luke: "<<luke<<endl;
-        int val= combat(luke);
-        //wait(nullptr);
-        exit(val);
+        return val;
     }
+    signal(SIGCHLD,handlerwait);
+    cout<<"vador: "<<vador<<", luke: "<<luke<<endl;
+    int val= combat(luke);
+    return val;
+
 }
