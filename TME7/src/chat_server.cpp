e39fd78453cd0c22ee05d5fc7bc9d2c87581d85c
Changrui <m.zhang.changrui@gmail.com>
2022-12-15 15:49:45 +0100

    communication client serveur

--- TME7/src/chat_server.cpp
+++ TME7/src/chat_server.cpp
@@ -6,80 +6,88 @@
 #include <algorithm>
 #define DEFAULTSERVER "sv"
 #define CONTENTSZ 1024
+#define MAXCLIENT 10
 using namespace std;
 myshm server;
 sem_t mutex;//simuler mutex
-vector<char*> clients;
-//3 operations
+char *clients[MAXCLIENT];
+int nbr_client=0;
+struct message *msg;
 
-//connexion
-void addClient(char* name){
-    sem_wait(&mutex);
-    clients.push_back(name);
-    sem_post(&mutex);
-}
-//déconnexion
-void delClient(char* name){
-    sem_wait(&mutex);
-    remove(clients.begin(),clients.end(),name);
-    clients.pop_back();
-    sem_post(&mutex);
-}
 //diffusion
-void diffusion(char* message){
-    for(char * client:clients){
-        int fd=shm_open(client,O_CREAT|O_EXCL|O_RDWR,0600);
-        ftruncate(fd,CONTENTSZ);
-        void *mem=mmap(0,CONTENTSZ,PROT_WRITE|PROT_READ,MAP_SHARED,fd,0);
-        char* m= new(mem) char();
-        m=message;
-        munmap(mem,CONTENTSZ);
-        sem_unlink(client);
+void diffusion(){
+    cout<<"message à diffuser: "<<msg->content<<endl;
+    for(int i=0;i<MAXCLIENT;i++){
+        if(clients[i]!=0){
+            //cout<<"diffusion ce client: "<<clients[0]<<endl;
+            int fd=shm_open(clients[i],O_RDWR,0600);
+            ftruncate(fd,CONTENTSZ);
+            void *mem=mmap(0,CONTENTSZ,PROT_WRITE|PROT_READ,MAP_SHARED,fd,0);
+            char* content= new(mem) char();
+            strcpy(content,msg->content);
+            munmap(mem,CONTENTSZ);
+            close(fd);
+            sem_t *client_sem= sem_open(clients[i],O_RDWR,0600,0);
+            sem_post(client_sem);
+            sem_close(client_sem);
+        }
     }
 }
 int main(int argc,char** argv){
+
     sem_init(&mutex,-1,1);
     const char* server_ID=(argv[1]==0)?DEFAULTSERVER:argv[1];
     int msz=sizeof(struct message);
-    cout<<server_ID<<endl;
+    cout<<"server: "<<server_ID<<endl;
     //creation espace partagé
     shm_unlink(server_ID);
     int fd= shm_open(server_ID,O_CREAT|O_RDWR|O_EXCL,0600);
     ftruncate(fd,msz);
     void * memoir=mmap(0,msz,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    message *msg=new(memoir) message;//msg dans une mémoire partagée
+    msg=new(memoir)struct message;//msg dans une mémoire partagée
     //----
     //init semaphore
     sem_unlink(server_ID);
     server.sem= sem_open(server_ID,O_CREAT|O_EXCL|O_RDWR,0600,0);
-
+    msg->type=122;
     //1 connexion,
     //2 diffusion de message,
     //3 déconnexion ;
     //connexion
     while(true){
         sem_wait(server.sem);
-
         ++server.read;
-
-        switch (msg->type) {
-            case 1:
-
-                addClient(msg->content);
-                break;
-            case 2:
-
-                diffusion(msg->content);
-                break;
-            case 3:
-
-                delClient(msg->content);
-                break;
-            default:
-
-                cout<<"msg non matched"<<endl;
+        cout<<"traitement type:"<<msg->type<<endl;
+        if(msg->type==1){//connexion
+            sem_wait(&mutex);
+            for(int i=0;i<MAXCLIENT;i++){
+                if(clients[i]==0){
+                    clients[i]=new char[strlen(msg->content)];
+                    strcpy(clients[i],msg->content);
+                    ++nbr_client;
+                    break;
+                }
+            }
+            sem_post(&mutex);
+        }else if(msg->type==2){//diffusioin
+            diffusion();
+        }else if(msg->type==3){//deconnexion
+            sem_wait(&mutex);
+            //free(clients[nbr_client]);
+            --nbr_client;
+            for(int i=0;i<MAXCLIENT;i++){
+                if(clients[i]!=0&&(strcmp(clients[i],msg->content)==0)){
+                    clients[i]=0;
+                    delete clients[i];
+                }
+            }
+            sem_post(&mutex);
+        }else{
+            cout<<"autre"<<endl;
         }
-
+        for(int i=0;i<nbr_client;i++){
+            cout<<"client: "<<clients[i]<<endl;
+        }
     }
 
     return 0;
